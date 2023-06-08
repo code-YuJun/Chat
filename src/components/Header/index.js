8979d4a31dd9cdc9002f2d9ad7bf445d0e64dc53
qiangyujun <qiangyujun@jd.com>
2023-06-08 13:40:22 +0800

    feat: 登录、未登录状态

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,25 @@
 import { PureComponent } from "react";
 import { connect } from "react-redux";
-import classnames from "classnames";
 import { HeaderWrapper } from "./style";
 // import { increment } from "@/store/festures/counterSlice.js";
 import { fetchLoginStateAction } from "@/store/login/login.js";
+import logoPic from "@/assets/img/chat.png";
 class Header extends PureComponent {
   componentDidMount() {
-    this.props.toLogin()
+    this.props.toLogin();
   }
   render() {
     const { value } = this.props;
     return (
       <HeaderWrapper>
         <div className="Logo">
-          <img src="" />
+          <img src={logoPic} alt="logo" />
         </div>
         <div className="Info">
           <div className="Name">聊天窗口</div>
           <div className="State">
-            {/* <svg>
-              <use xlink:href=""></use>
+            {/* <svg width={'10px'} height={'10px'} fill={'red'}>
+              <use xlinkHref={"#loginFail"}></use>
             </svg> */}
           </div>
         </div>
@@ -27,11 +27,13 @@ class Header extends PureComponent {
     );
   }
 }
-const mapStateToProps = () => {};
+const mapStateToProps = (state) => ({
+  value: state.counter.value,
+});
 // 便利 redux 中的方法
 const mapDispatchToProps = (dispatch) => ({
-  toLogin(){
-    dispatch(fetchLoginStateAction())
-  }
+  toLogin() {
+    dispatch(fetchLoginStateAction());
+  },
 });
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
