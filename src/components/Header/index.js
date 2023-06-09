2bd48a3de6a14dc01bcf7446c14795ea72252b65
qiangyujun <qiangyujun@jd.com>
2023-06-09 13:42:47 +0800

    feat: 顶部样式

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,9 @@
 import { PureComponent } from "react";
 import { connect } from "react-redux";
 import { HeaderWrapper } from "./style";
-// import { increment } from "@/store/festures/counterSlice.js";
 import { fetchLoginStateAction } from "@/store/login/login.js";
+import logining from "@/assets/svg/logining.svg"
+import loginFail from "@/assets/svg/loginFail.svg"
 import logoPic from "@/assets/img/chat.png";
 class Header extends PureComponent {
   componentDidMount() {
@@ -13,15 +14,11 @@ class Header extends PureComponent {
     return (
       <HeaderWrapper>
         <div className="Logo">
-          <img src={logoPic} alt="logo" />
+          <img src={logoPic} alt="logo" className="Logo_name"/>
         </div>
         <div className="Info">
-          <div className="Name">聊天窗口</div>
-          <div className="State">
-            {/* <svg width={'10px'} height={'10px'} fill={'red'}>
-              <use xlinkHref={"#loginFail"}></use>
-            </svg> */}
-          </div>
+          <div className="Info_Name">聊天窗口</div>
+          <img src={ logining } className="Info_State"></img>
         </div>
       </HeaderWrapper>
     );
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,4 +1,28 @@
 import styled from "styled-components"
-export const HeaderWrapper  = styled.div`
+export const HeaderWrapper = styled.div`
+    height: 60px;
+    display:flex;
     color:red;
+    padding:10px;
+    .Logo{
+        width:40px;
+        height:40px;
+    }
+    .Logo_name{
+        width:100%;
+        height:100%;
+    }
+    .Info{
+        display:flex;
+        flex-direction:column;
+        margin-left:10px;
+    }
+    .Info_Name{
+        font-size:14px;
+        color:#343036;
+    }
+    .Info_State{
+        width:10px;
+        height:10px;
+    }
 `
